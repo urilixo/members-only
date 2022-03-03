490160338a315e21d6fb492b43c5435a0b7080a6
urilixo <falecomelvio@hotmail.com>
2022-03-03 12:04:33 -0300

    Add support for username in sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {registrations: :registrations }
   resources :users
   resources :posts, only: %i[new create index]
-  root to: 'home#index'
+  root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
