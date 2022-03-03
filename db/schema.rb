868328ba0425be904933509de4e17c67586d3345
urilixo <falecomelvio@hotmail.com>
2022-03-03 11:23:32 -0300

    Add layout to sign up view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_203230) do
+ActiveRecord::Schema.define(version: 2022_03_02_221258) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
